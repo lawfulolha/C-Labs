5592d223c9d8aea29e6640c3db51e7040fae0e12
lawfulolha <56075974+lawfulolha@users.noreply.github.com>
2020-06-05 09:38:03 +0300

    Add files via upload

---
+++ Lab7/Lab7/MatrixMultiplier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    class MatrixMultiplier
+    {
+        public int[,] First { get; set; }
+        public int[,] Second { get; set; }
+        public int[,] Result { get; private set; }
+
+        public MatrixMultiplier(int[,] first, int[,] second)
+        {
+            First = first;
+            Second = second;
+            Result = new int[first.GetLength(1), second.GetLength(0)];
+        }
+
+        public void GenerateFirstMatrix(int rows, int columns)
+        {
+            First = new int[rows, columns];
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    First[i, j] = random.Next(-100, 100);
+                }
+            }
+        }
+
+        public void GenerateSecondMatrix(int rows, int columns)
+        {
+            Second = new int[rows, columns];
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Second[i, j] = random.Next(-100, 100);
+                }
+            }
+        }
+
+        public bool CheckMultiply()
+        {
+            if (First.GetLength(1) == Second.GetLength(0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void MultiplyElement(int i, int j)
+        {
+            Result[i, j] = 0; 
+            for (int k = 0; k < this.First.GetLength(0); ++k)
+            {
+                this.Result[i, j] += this.First[i, k] * this.Second[k, j];
+            }
+        }
+
+
+    }
+}
---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nRows, mRows, nColumns, mColumns;
+            Console.WriteLine("First matrix size:");
+            Console.Write("Enter number of rows: ");
+            nRows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number of columns: ");
+            nColumns = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second matrix size: ");
+            Console.Write("Enter number of rows: ");
+            mRows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number of columns: ");
+            mColumns = Convert.ToInt32(Console.ReadLine());
+
+            MatrixMultiplier multiplier = new MatrixMultiplier(new int[nRows, nColumns], new int[mRows, mColumns]);
+
+            multiplier.GenerateFirstMatrix(nRows, nColumns);
+            multiplier.GenerateSecondMatrix(mRows, mColumns);
+
+            if (!multiplier.CheckMultiply())
+            {
+                Console.WriteLine("Cannot multiply matrices because of the sizes");
+                return;
+            }
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < nRows; i++)
+            {
+                for (int j = 0; j < mColumns; j++)
+                {
+                    threads.Add(new Thread(() => multiplier.MultiplyElement(i, j)));
+                }
+            }
+
+            foreach (var n in threads)
+            {
+                n.Start();
+            }
+
+            foreach (var n in threads)
+            {
+                n.Join();
+            }
+
+            for (int i = 0; i < multiplier.Result.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < multiplier.Result.GetLength(1); j++)
+                {
+                    Console.Write(multiplier.Result[i, j]);
+                }
+            }
+        }
+    }
+}
